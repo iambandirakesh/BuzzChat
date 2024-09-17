4a5aea5a88020809b97b301bd244c7d1224b3728
iambandirakesh <brr9384@GMAIL.COM>
2024-09-17 12:05:58 +0530

    fetching user details issue

--- server/middlewares/Auth.middleware.js
+++ server/middlewares/Auth.middleware.js
@@ -2,7 +2,8 @@ import jwt from "jsonwebtoken";
 import UserModel from "../models/user.model.js";
 
 export const AuthMiddleware = async (req, res, next) => {
-  const token = req.cookies.token || "";
+  console.log(req.cookies.token);
+  const token = req.cookies.token || localStorage.getItem("token") || "";
   try {
     if (!token) {
       return res.status(401).json({
